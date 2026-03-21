fca0e66a1fac3a2884f25b508da40141bb4888f1
miloetz <111991053+miloetz@users.noreply.github.com>
2026-03-20 20:49:17 -0500

    added logo icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "TheGreenRoom",
   description: "Gigging Platform",
+  icons: {
+    icon: "/thegreenroomicon.png",
+  },
 };
 
 export default function RootLayout({
